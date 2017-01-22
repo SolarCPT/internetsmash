0f93b09e21f60de9fb814e4b03951a3d5451dd92
SolarCPT <sinchai.sapakdee@gmail.com>
2017-01-23 01:25:57 +0700

    Update TimeAction.js

--- TimeAction.js
+++ TimeAction.js
@@ -18,34 +18,25 @@ function ShopStatus(msg,work=0){
      var work=work;
      var act ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(0, 100, 255);>\">Today Open</span><span style=\"color:rgb(0, 206, 0);\"><b> !!</b></span>";
      var unact ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">Now Closed</span><span style=\"color:red;\"><b> !!</b></span>";
-     var currentDate = new Date();
-     var currentHour = currentDate.getHours();
-     var currentMinute = currentDate.getMinutes();
-     var currentSecond = currentDate.getSeconds();
- if(currentHour > 12)
-     currentHour = currentHour % 12;
- if(currentHour <= 12)
-    currentHour = currentHour;
-     currentHour= currentHour ? currentHour : 12; // the hour '0' should be '12'
-     var ampm=currentHour <= 12 ? 'PM' : 'AM';
-     currentMinute = currentMinute < 10 ? '0'+currentMinute : currentMinute;
-     currentSecond = currentSecond <10 ? '0'+currentSecond : currentSecond;
-    // add a zero in front of numbers<10
-   if(work==1){
-   	document.write(msg);
-   }else{
-   	if(currentHour>12)ampm="PM";
-   	if(ampm == "PM" && currentHour < 7){
-   		document.write(act);
-   		 }else if(ampm == "PM" && currentHour > 11){
-   		 	document.write(act+ampm);
-   		 	 }else if(ampm == "PM" && currentHour > 6 ){
-   		 	 	document.write(unact);
-   		 	 	 }else if(ampm == "AM" && currentHour >=9){
-   		 	 	 	document.write(act);
-   		 	 	 	 }else{
-   		 	 	 	 	document.write(unact+" Test Script"+ampm+" YES");
-   		 	 	 	 }
-   		 	 	 	}
-   		 	 	  }
+     var date_obj = new Date();
+     var hour = date_obj.getHours();
+     var minute = date_obj.getMinutes();
+     var amPM = (hour > 11) ? "pm" : "am";
+  if(hour > 12) {
+    hour -= 12;
+  } else if(hour == 0) {
+    hour = "12";
+  }
+  if(minute < 10) {
+    minute = "0" + minute;
+  }
+  if(amPM =="am" && hour > 8){
+    document.write(act);
+  }else{
+    document.write(unact);
+  }
+  if(amPM == "pm" && hour < 7){
+    document.write(act);
+  }
+ }
    //]]>
