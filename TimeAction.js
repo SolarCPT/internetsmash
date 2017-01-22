2fdfa1ddfb8d8a531e75208b34246e15464e559c
SolarCPT <sinchai.sapakdee@gmail.com>
2017-01-22 16:29:19 +0700

    Update TimeAction.js

--- TimeAction.js
+++ TimeAction.js
@@ -16,7 +16,7 @@
 function ShopStatus(msg,work=0){
      var msg="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">"+msg+"</span><span style=\"color:red;\"><b> !!</b></span>";
      var work=work;
-     var act ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(0, 100, 255);>\">Today Open (:</span><span style=\"color:rgb(0, 206, 0);\"><b> !!</b></span>";
+     var act ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(0, 100, 255);>\">Today Open</span><span style=\"color:rgb(0, 206, 0);\"><b> !!</b></span>";
      var unact ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">Now Closed</span><span style=\"color:red;\"><b> !!</b></span>";
      var currentDate = new Date();
      var currentHour = currentDate.getHours();
