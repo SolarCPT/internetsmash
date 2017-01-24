755a8a2b48afb38893454829fb170abfb2b90406
SolarCPT <sinchai.sapakdee@gmail.com>
2017-01-24 17:08:14 +0700

    Update TimeAction.js

--- TimeAction.js
+++ TimeAction.js
@@ -19,25 +19,29 @@ function ShopStatus(msg,work=0){
      var act ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(0, 100, 255);>\">Today Open</span><span style=\"color:rgb(0, 206, 0);\"><b> !!</b></span>";
      var unact ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">Now Closed</span><span style=\"color:red;\"><b> !!</b></span>";
      var date_obj = new Date();
-     var hour = date_obj.getHours();
+     var hour   = date_obj.getHours();
      var minute = date_obj.getMinutes();
-     var amPM = (hour > 11) ? "pm" : "am";
-  if(hour > 12) {
-    hour -= 12;
-  } else if(hour == 0) {
-    hour = "12";
-  }
-  if(minute < 10) {
-    minute = "0" + minute;
-  }
-   if(amPM =="am" && hour > 8){
-    document.write(act);
-  }else if(amPM == "am" && hour < 9){
-    document.write(unact);
-  }else if(amPM == "pm" && hour < 7){
-    document.write(act);
-  }else if(amPM == "pm" && hour > 6){
-    document.write(unact);
-  }
+     var amPM   = (hour > 11) ? "pm" : "am";
+     if(work==1){
+        document.write(msg);
+     }else{
+     if(hour > 12) {
+     hour -= 12;
+     } else if(hour == 0) {
+     hour = "12";
+     }
+     if(minute < 10) {
+     minute = "0" + minute;
+     }
+     if(amPM =="am" && hour > 8){
+     document.write(act);
+     }else if(amPM == "am" && hour < 9){
+     document.write(unact);
+     }else if(amPM == "pm" && hour < 7){
+     document.write(act);
+     }else if(amPM == "pm" && hour > 6){
+     document.write(unact);
+     }
  }
+     }
    //]]>
