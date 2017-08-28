20f07cc106c398ed8d150e394d90a5dc652fad6c
SolarCPT <sinchai.sapakdee@gmail.com>
2017-08-28 15:19:44 +0700

    Update TimeAction.js

--- TimeAction.js
+++ TimeAction.js
@@ -23,7 +23,7 @@ function ShopStatus(YourMessage,work=0){
      var amPM   = (hour > 11) ? "pm" : "am";
      if(work==1){
      var msg="Now Close!!";
-     var Newmsg="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">"+msg+" "+YourMessage+"</span><span style=\"color:red;\"></span>";
+     var Newmsg="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">"+msg+"<br/>"+YourMessage+"</span><span style=\"color:red;\"></span>";
         document.write(Newmsg);
      }
      }else{
