4613044306e76389661c6f1c68381b9614774e75
SolarCPT <sinchai.sapakdee@gmail.com>
2017-09-13 14:00:33 +0700

    Update TimeAction.js

--- TimeAction.js
+++ TimeAction.js
@@ -1,2 +1,46 @@
 
      
+ function displayTime()
+ //<![CDATA[
+    {
+     var currentDate = new Date();
+     var currentHour = currentDate.getHours();
+     var currentMinute = currentDate.getMinutes();
+     var currentSecond = currentDate.getSeconds();
+     //currentHour = currentHour % 12;
+     //currentHour= currentHour ? currentHour : 12; // the hour '0' should be '12'
+     //var ampm=currentHour <= 12 ? 'PM' : 'AM';
+     currentMinute = currentMinute < 10 ? '0'+currentMinute : currentMinute;
+     currentSecond = currentSecond <10 ? '0'+currentSecond : currentSecond;
+     document.getElementById('timeDiv').innerHTML =currentHour + ':' + currentMinute + ' : ' + currentSecond;
+     t=setTimeout(function(){displayTime()},700);
+    }
+ function ShopStatus(msg,YourMessage,work=0){
+     var msg=msg;
+     var work=work;
+     var act ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(0, 100, 255);>\">Today Open</span><span style=\"color:rgb(0, 206, 0);\"><b> !!</b></span>";
+     var unact ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">Now Closed</span><span style=\"color:red;\"><b> !!</b></span>";
+     var date_obj = new Date();
+     var hour   = date_obj.getHours();
+     var minute = date_obj.getMinutes();
+     var amPM   = (hour > 11) ? "pm" : "am";
+     if(work == 1){
+        var Nmsg=msg;
+        var Newmsg="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">"+Nmsg+"<br/>"+YourMessage+"</span><span style=\"color:red;\"></span>";
+        document.write(Newmsg);
+     }else{
+     if(hour > 11) {
+     hour -= 12;
+     }if(hour == 0){
+      hour = 12;
+         }
+         else if(amPM=="am" && hour == 0) {
+      document.write(unact);
+     }else if(amPM =="am" && hour > 8 && hour < 12){
+     document.write(act);
+     }else if(amPM == "pm" && hour >6 && hour < 12 ){
+     document.write(unact);
+     }
+      
+     }
+//]]>
