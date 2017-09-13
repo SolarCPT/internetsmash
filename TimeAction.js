54963fcbe4a45366b3b5d9cbb58bdb57e630997e
SolarCPT <sinchai.sapakdee@gmail.com>
2017-09-13 09:41:07 +0700

    Update TimeAction.js

--- TimeAction.js
+++ TimeAction.js
@@ -39,8 +39,6 @@ function ShopStatus(msg,YourMessage,work=0){
      document.write(act);
      }else if(amPM == "am" && hour < 9){
      document.write(unact);
-     }else if(amPM == "pm" && hour > 11){
-      document.write(act);
      }else if(amPM == "pm" && hour < 7){
      document.write(act);
      }else if(amPM == "pm" && hour > 6){
