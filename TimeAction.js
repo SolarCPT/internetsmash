fd40c75f9fcc7fcda90154fbd5598c929a407e9a
SolarCPT <sinchai.sapakdee@gmail.com>
2017-09-19 18:56:27 +0700

    Update TimeAction.js

--- TimeAction.js
+++ TimeAction.js
@@ -1,10 +1,11 @@
 function ShopStatus(msg,w="0"){
-msg = msg;
+var msg = msg;
 day = new Date();
 hr = day.getHours();
 var ampm=(hr >11) ? 'PM' : 'AM';
 var act ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(0, 100, 255);>\">Today Open</span><span style=\"color:rgb(0, 206, 0);\"><b> !!</b></span>";
 var unact ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">Now Closed</span><span style=\"color:red;\"><b> !!</b></span>";
+var msg ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">"+msg+"</span><span style=\"color:red;\"><b> !!</b></span>";
 if(w==1){
 document.write(msg);
 }else{
