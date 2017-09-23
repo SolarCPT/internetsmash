2ba62dd0e641ed85b30102bb455b3b555c6c0aa7
SolarCPT <sinchai.sapakdee@gmail.com>
2017-09-23 15:23:24 +0700

    Update TimeAction.js

--- TimeAction.js
+++ TimeAction.js
@@ -1,25 +1,30 @@
 function ShopStatus(msg,w="0"){
-var msg = msg;
-day = new Date();
-hr = day.getHours();
-var ampm=(hr >11) ? 'PM' : 'AM';
-var act ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(0, 100, 255);>\">Today Open</span><span style=\"color:rgb(0, 206, 0);\"><b> !!</b></span>";
-var unact ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">Now Closed</span><span style=\"color:red;\"><b> !!</b></span>";
-msg ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">"+msg+"</span><span style=\"color:red;\"><b> !!</b></span>";
-if(w==1){
-document.write(msg);
-}if(hr>12){
-	hr-=12;
-}else if(hr==0){
-	hr=12;	
-	document.write(unact);
-}else if(ampm === "PM" && hr < 7 ){
-	document.write(act);
-}else if(ampm === "AM" && hr > 8){
-	document.write(act);
-}else if(ampm === "AM" && hr < 9){
-	document.write(unact);
-}else if(ampm === "PM" && hr === 12){
-	document.write(act);
-}
+	var msg = msg;
+	day = new Date();
+	hr = day.getHours();
+	var hrs =(hr>12)? 24 : 12;
+	var ampm="";
+	var act ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(0, 100, 255);>\">Today Open</span><span style=\"color:rgb(0, 206, 0);\"><b> !!</b></span>";
+	var unact ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">Now Closed</span><span style=\"color:red;\"><b> !!</b></span>";
+	msg ="<b>STATUS</b><span style=\"color:red;\"><b> :: </b></span><span style=\"color:rgb(255, 0, 29);\">"+msg+"</span><span style=\"color:red;\"><b> !!</b></span>";
+	if(w==1){
+		document.write(msg);
+	}else{
+		if(hrs==24){
+			ampm = "PM";
+			hr-=12;
+		}else if(ampm=="PM" && hr<7){
+			 document.write(act);
+		}else{
+			document.write(unact);
+		}
+		if(hrs==12){
+			ampm="AM";
+			hr=hr;
+		}else if(ampm=="AM" && hr>8){
+			document.write(act);
+		}else{
+			document.write(unact);
+		}
+	}
 }
